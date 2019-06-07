beb877dc62ff55b79c01034d40645d2dd2a785e9
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-06-07 12:30:39 -0400

    inverse

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CS441-prog1
+
+This app presents the user with 6 buttons(five at the top of the screen and one at the bottom). The upper five buttons each make one of the five letters of HELLO visible or invisible in the color of the button.
+The bottom button changes the color of each letter to be the complementary color of their respective button.
+
+After figuring out the basics of android studio, I had originally wanted to create an app with only one button that generates another button. The new button generating another button and then another button, etc. These buttons would eventually be positioned in a way to spell out HELLO. 
+Unfortunately I couldn't quite figure out how to do this so I settled on having the buttons just show letters. 
+
+5/30- created repository and added app to repository
+
+5/31- edited the hello world text to say Hi and attempted to change its font size
+
+6/3- created a button that put the word "Mail" in the snackbar
+
+6/6- deleted old buttons and text and added in five new buttons of various colors that made five letters visible when pressed
+
+6/7- added additional button that changed the colors of the five letters to their complements
--- app/src/main/java/com/example/cs441_p1/MainActivity.java
+++ app/src/main/java/com/example/cs441_p1/MainActivity.java
@@ -9,9 +9,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Switch;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public boolean on;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,45 +25,75 @@ public class MainActivity extends AppCompatActivity {
         final TextView t3=(TextView)findViewById(R.id.textView3);
         final TextView t4=(TextView)findViewById(R.id.textView4);
         final TextView t5=(TextView)findViewById(R.id.textView5);
+        Button i=(Button) findViewById(R.id.button);
         FloatingActionButton f = findViewById(R.id.f);
         FloatingActionButton f2 = findViewById(R.id.f2);
         FloatingActionButton f3 = findViewById(R.id.f3);
         FloatingActionButton f4 = findViewById(R.id.f4);
         FloatingActionButton f5 = findViewById(R.id.f5);
+        on=false;
+
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(on){
+                    t.setTextColor(0xFFFF9800);
+                    t2.setTextColor(0xFF3F51B5);
+                    t3.setTextColor(0xFF4CAF50);
+                    t4.setTextColor(0xFFFF0000);
+                    t5.setTextColor(0xFF9C27B0);
+                    on=false;
+                }else{
+                    t.setTextColor(0xFF0067FF);
+                    t2.setTextColor(0xFFB5A33F);
+                    t3.setTextColor(0xFFAF4CAB);
+                    t4.setTextColor(0xFF00FFFF);
+                    t5.setTextColor(0xFF3BB027);
+                    on=true;
+                }
+
+            }
+        });
+
+
         f.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t.setVisibility((t.getVisibility() ==View.VISIBLE) ? View.GONE : View.VISIBLE);
+                t.setVisibility((t.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
             }
         });
 
         f2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t2.setVisibility((t2.getVisibility() ==View.VISIBLE) ? View.GONE : View.VISIBLE);
+                t2.setVisibility((t2.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
             }
         });
 
         f3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t3.setVisibility((t3.getVisibility() ==View.VISIBLE) ? View.GONE : View.VISIBLE);
+                t3.setVisibility((t3.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
+
             }
         });
 
         f4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t4.setVisibility((t4.getVisibility() ==View.VISIBLE) ? View.GONE : View.VISIBLE);
+                t4.setVisibility((t4.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
+
             }
         });
 
         f5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t5.setVisibility((t5.getVisibility() ==View.VISIBLE) ? View.GONE : View.VISIBLE);
+                t5.setVisibility((t5.getVisibility() == View.VISIBLE) ? View.GONE : View.VISIBLE);
+
             }
         });
+
     }
 
     @Override
